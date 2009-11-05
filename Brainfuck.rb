28efaeb7b34dfa00ed8959179ce483eb4fa427e5
Jennifer Freeman <jen.a.freeman@googlemail.com>
2009-11-05 23:52:51 +0000

    Implement [

--- Brainfuck.rb
+++ Brainfuck.rb
@@ -6,9 +6,23 @@ core :Brainfuck "A Brainfuck CPU" do
   opcode "." { putc mem[dp] }
   opcode "," { mem[dp] = STDIN.getc }
   opcode "[" do |x|
-    # Urgh
+    if (0 == mem[dp])
+      count = 1
+      while x != 0
+        pc +=1
+        if mem[pc]== 91 # [
+          count+=1
+        elsif mem[pc]== 93 # ]
+          count-=1
+        end
+      end
+    end
   end
   opcode "]" do |x|
     # Urgh
   end
+
+  memsize :name=>mem,:size=>30000 # Classical brainfuck
+  pc :name=>:pc,:start=0
+  register :name=>:dp,:var=>:dp
 end
