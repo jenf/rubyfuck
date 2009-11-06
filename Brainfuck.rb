91a4d06aa548fceef34a89e714d89e2ef6922437
Jennifer Freeman <jen.a.freeman@googlemail.com>
2009-11-06 21:45:42 +0000

    Changes to style

--- Brainfuck.rb
+++ Brainfuck.rb
@@ -1,39 +1,38 @@
-core :Brainfuck=>"A Brainfuck CPU" do
-  opcode ">" do dp+=1 end
-  opcode "<" do dp-=1 end
-  opcode "+" do mem[dp]+=1 end
-  opcode "-" do mem[dp]-=1 end
-  opcode "." do putc mem[dp] end
-  opcode "," do mem[dp] = STDIN.getc end
-  opcode "[" do
-    if (0 == mem[dp])
-      count = 1
-      while count != 0
-        pc +=1
-        if mem[pc]== 91 # [
-          count+=1
-        elsif mem[pc]== 93 # ]
-          count-=1
-        end
+opcode ">" do dp+=1 end
+opcode "<" do dp-=1 end
+opcode "+" do mem[dp]+=1 end
+opcode "-" do mem[dp]-=1 end
+opcode "." do putc mem[dp] end
+opcode "," do mem[dp] = STDIN.getc end
+opcode "[" do
+  if (0 == mem[dp])
+    count = 1
+    while count != 0
+      pc +=1
+      if mem[pc]== 91 # [
+        count+=1
+      elsif mem[pc]== 93 # ]
+        count-=1
       end
     end
   end
-  opcode "]" do
-    if (0 == mem[dp])
-      count = 1
-      while count != 0
-        pc -=1
-        if mem[pc]== 91 # [
-          count-=1
-        elsif mem[pc]== 93 # ]
-          count+=1
-        end
-      end
-    end
-    :no_inc_pc
-  end
-
-  memsize :name=>mem,:size=>30000 # Classical brainfuck
-  pc :name=>:pc,:start=>0
-  register :name=>:dp,:var=>:dp
 end
+opcode "]" do
+  if (0 == mem[dp])
+    count = 1
+    while count != 0
+      pc -=1
+      if mem[pc]== 91 # [
+        count-=1
+      elsif mem[pc]== 93 # ]
+        count+=1
+      end
+    end
+  end
+  :no_inc_pc
+end
+
+memsize :name=>:mem,:size=>30000 # Classical brainfuck
+pc :name=>:pc,:start=>0
+register :name=>:dp,:var=>:dp
+rom :file=>ARGV[1],:invisible_codespace=>true
--- CPUCore.rb
+++ CPUCore.rb
@@ -1,5 +1,48 @@
-class CoreCPU
+class CPUCore
+ def initialize()
+  @core = CPULoader.new()
+  @core.load(ARGV[0])
+ end
+ def begin
+  puts "Start!"
+ end
 end
 
-class RubyFuck
+class CPULoader
+ def initialize()
+ end
+ 
+ def load(*args, &block)
+  # Convert a parameter to a hash.
+  options = args.last.is_a?(Hash) ? args.pop : {}
+  args.each { |arg| load options.merge(:file=>arg) }
+  return unless args.empty?
+  
+  if options[:file]
+   file = options[:file]
+   load :string => File.read(file), :name => options[:name] || file
+  elsif options[:string]
+   instance_eval(options[:string], options[:name]|| "<eval>")
+  end
+ end
+ 
+ def opcode(*args,&block)
+  options = args.last.is_a?(Hash) ? args.pop : {}
+  args.each { |arg| opcode options.merge(:code=>arg) }
+  return unless args.empty?
+  puts "Defining opcode " + (options.inspect)
+ end
+ 
+ def memsize(args)
+  puts args.inspect
+ end
+ def pc(args)
+  puts args.inspect
+ end
+ def register(args)
+  puts args.inspect
+ end
+ def rom(args)
+  puts args.inspect
+ end
 end
